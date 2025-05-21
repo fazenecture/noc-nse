29ccf6a65909774dc82dd33c22c41cf997cc33f9
fazenecture <vivekkumarsingh2002@gmail.com>
2025-05-21 16:07:40 +0530

    feat: testing

--- src/service/cron.service.ts
+++ src/service/cron.service.ts
@@ -11,7 +11,7 @@ export default class CRONService {
   public execute = async () => {
     try {
       console.log("CRON: INIT");
-      cron.schedule("* * * * *", () => {
+      cron.schedule("2 * * * *", () => {
         // cron.schedule("30 15 * * *", () => {
         try {
           console.log("CRON: Starting");
